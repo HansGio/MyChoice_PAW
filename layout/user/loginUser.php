feb549f5fdabe6e44d31a47272456ae65b7aa428
KevinGhebre <kevinghebre20@gmail.com>
2020-09-28 21:11:22 +0700

    update profil

--- layout/user/loginUser.php
+++ layout/user/loginUser.php
@@ -8,21 +8,17 @@ include '../../index.php'
             <h1 class="text-center">Log In</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                <label for="email_input">Email address</label>
+                <input type="email" class="form-control" id="email_login" aria-describedby="emailHelp"
                     placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                     else.</small>
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <label for="password_input">Password</label>
+                <input type="password" class="form-control" id="password_login" placeholder="Password">
             </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Log In</button>
         </form>
     </div>
 </div>
