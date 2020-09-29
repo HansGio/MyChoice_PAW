e7ee04a972a81eea115e4275b4ddb299949a9f3f
KevinGhebre <kevinghebre20@gmail.com>
2020-09-30 02:04:08 +0700

    .

--- layout/dashboardUser.php
+++ layout/dashboardUser.php
@@ -1,7 +1,7 @@
 <?php
 include '../db.php'
 ?>
-<?php echo '
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,7 +10,8 @@ include '../db.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!--    INI FRAMEWORK BOOTSRAP-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
 
     <link type="text/css" rel="stylesheet" href="../css/style.css" />
     <link rel="icon" type="image/png" href="../img/favicon/favicon.png" />
@@ -19,11 +20,14 @@ include '../db.php'
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 
     <!--   SCRIPT     -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
     </script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>MyChoice</title>
@@ -33,7 +37,8 @@ include '../db.php'
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light border-bottom fi">
         <div class="container">
             <a href="#"><img style="height: 40px" src="../img/logo/logo.png" alt="Logo" /></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -56,10 +61,10 @@ include '../db.php'
 						"></div>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Hi, Name <i class="fas fa-user-circle fa-lg" aria-hidden="true"></i></a>
+                        <a class="nav-link" href="#">Hi, Name <i class="fas fa-user-circle fa-lg"
+                                aria-hidden="true"></i></a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-' ?>
