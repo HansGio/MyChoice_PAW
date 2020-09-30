35025fa1674c0277f736fd9561743b29d6128596
Diky <juliusdikya@gmail.com>
2020-09-30 23:34:50 +0700

    update contact us

--- layoutUser/contactUsUser.php
+++ layoutUser/contactUsUser.php
@@ -1,3 +1,54 @@
 <?php
 include '../dashboard/dashboardUser.php'
 ?>
+
+<div class="container content">
+<div class="containercolumnis-10">
+    <div class="section">
+        <h1 class="title">Contact Us</h1>
+        <h5> Monday - Saturday (08.30 - 17.30) </h5>
+        <form action="" method="post">
+            <div class="form-group col-xs4">
+                <label class="label">Name</label>
+                <div class="control col-xs-4">
+                    <input name="name" class="form-control" type="text" placeholder="Name">
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <label class="label">Email</label>
+                        <div class="control">
+                            <input name="email" class="form-control" type="email" placeholder="Email">
+                            </div>
+                        </div>
+                                           
+                        <div class="form-group">
+                            <label class="label">Phone Number</label>
+                            <div class="control">
+                                <input name="phone" class="form-control" type="number" placeholder="Phone Number">   
+                            </div>
+                        </div>
+                    
+                        <div class="form-group">
+                            <label class="label">Message</label>
+                            <div class="control">
+                                <input name="message" class="form-control" type="text" placeholder="Type your message here">
+                            </div>
+                        </div>
+                        
+                        <div class="field is-grouped">
+                            <div class="control">
+                                <button name="" Type="button" class="btn btn-primary">Submit</button>
+                            </div>
+                        
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+</div>
+</form>
+
+
+
+</html>
