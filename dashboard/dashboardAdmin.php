d2975771b480f435c316d409c22e375c7ca31e66
KevinGhebre <kevinghebre20@gmail.com>
2020-09-30 22:26:35 +0700

    .

--- dashboard/dashboardAdmin.php
+++ dashboard/dashboardAdmin.php
@@ -10,7 +10,6 @@ include '../db.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-
     <!-- STYLE CSS -->
     <link rel="stylesheet" href="../css/styleAdmin.css">
 
@@ -46,6 +45,7 @@ include '../db.php'
                 </a>
             </div>
         </div>
+        <script src="../js/script.js"></script>
         <div class="content">
             <div class="header">
                 <div class="hamburger" onclick="toogleSidebar()">
@@ -53,33 +53,3 @@ include '../db.php'
                     <div></div>
                     <div></div>
                 </div>
-                <div class="title">
-                    <h2>Daftar Produk</h2>
-                </div>
-            </div>
-            <div class="body">
-                <div class="big-menu">
-                    <h4 class="active-big-menu"> Semua Produk</h4>
-                    <h4>Akrif</h4>
-                    <h4>NonAktif</h4>
-                    <input type="text" name="searchAllItemFromAdmin" id="search" placeholder="Search">
-
-                    <!-- <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Recipient's username"
-                            aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-                        </div>
-                    </div> -->
-
-                    <!-- </div>
-                    <div class="search"> -->
-                </div>
-                <table class="table table-hover">
-                </table>
-            </div>
-        </div>
-    </div>
-    </div>
-
-    <script src="../js/script.js"></script>
