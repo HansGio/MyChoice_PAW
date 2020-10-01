7b3a02186cefdd830688a27064d84c3924c847fd
KevinGhebre <kevinghebre20@gmail.com>
2020-10-02 00:22:11 +0700

    pull vel

--- layoutUser/aboutUser.php
+++ layoutUser/aboutUser.php
@@ -2,21 +2,22 @@
 include '../dashboard/dashboardUser.php'
 ?>
 
+<!-- ini test pull buat vellin -->
 <div class="container content">
-  <div class="row row-cols-2">
-    <div class="col">
-        <img style="width:100%; height:100%" src="../img/5aa737ee-d142-476f-b3d8-e863e2b56648-getty-688398000.jpg">
-    </div>
-    <div class="col">
-      <h2> HI, WELCOME TO MyChoice </h2>
-      <p class="about"> MyChoice is a new local brand in 2020 by Our Team. 
-          Our clothes made with premium material and high quality product.
-          The clothes designed by following the fashion trend around the 
-          world to serve the best experience to customer. 
-          We hope you love our product.</p>
+    <div class="row row-cols-2">
+        <div class="col">
+            <img style="width:100%; height:100%" src="../img/5aa737ee-d142-476f-b3d8-e863e2b56648-getty-688398000.jpg">
+        </div>
+        <div class="col">
+            <h2> HI, WELCOME TO MyChoice </h2>
+            <p class="about"> MyChoice is a new local brand in 2020 by Our Team.
+                Our clothes made with premium material and high quality product.
+                The clothes designed by following the fashion trend around the
+                world to serve the best experience to customer.
+                We hope you love our product.</p>
 
-      <p class="about"> Thank you for shopping and trusting us </p>
+            <p class="about"> Thank you for shopping and trusting us </p>
+        </div>
     </div>
-  </div>
 
-</html>
+    </html>
