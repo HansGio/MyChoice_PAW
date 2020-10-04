a9639f575734d20317ec24a1b1607bb8e872b77f
KevinGhebre <kevinghebre20@gmail.com>
2020-10-04 15:51:01 +0700

    tambahan layout untuk man dan women item list

---
+++ layoutUser/manListItemUser.php
@@ -0,0 +1,150 @@
+<?php
+include '../dashboard/dashboardUser.php'
+?>
+<link rel="stylesheet" href="../css/checkOutUser.css">
+<br><br><br><br><br><br><br>
+
+<div class="container" style="background-color:#D8DEE5 ;">
+    <div class="head d-flex">
+        <div class="back">
+            <button style="background-color:#D8DEE5 ;"><i class="fas fa-reply"></i></button>
+        </div>
+        <div class="text-center col-sm-2 offset-sm-5  "
+            style="height: 100px; background-color: #5FB5EA; color: #FFFFFF;">
+            <h2 class="align-self-center" style="margin-top: 20px;"> Man </h2>
+        </div>
+
+        <div class="input-group offset-sm-1 col-sm-3 align-self-center"
+            style=" background-color: white; border: 1px solid black;">
+            <input type="text" class="form-control" placeholder="Item Name" aria-label="Recipient's username"
+                aria-describedby="button-addon2" style="border: none;">
+            <div class="input-group-append">
+                <button class="btn" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+
+    <!-- Ini bagian Item nya.. yang atas udah fix  -->
+    <!-- Ini masih pake data dummmy... ntr di loop aja bagian itu -->
+
+    <div class="body d-flex flex-wrap">
+
+        <!-- dari sini -->
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <!-- sampe sini loop -->
+
+
+
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+        <div class="items">
+            <img src="../img/vIWkY3LiPq.jpg" alt="">
+            <div class="details ">
+                <h4 class="text-center">
+                    Kaos Putih<br>
+                    S/M/L/XL <br>
+                    Harga.. <br>
+                    <br>
+                    <br>
+                </h4>
+            </div>
+        </div>
+
+
+
+
+    </div>
