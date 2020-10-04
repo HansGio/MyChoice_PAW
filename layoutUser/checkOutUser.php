ddbb16af4de708fe217c3225ac5c3788e6e46038
KevinGhebre <kevinghebre20@gmail.com>
2020-10-04 14:55:39 +0700

    perbaikan

--- layoutUser/checkOutUser.php
+++ layoutUser/checkOutUser.php
@@ -7,7 +7,7 @@ include '../dashboard/dashboardUser.php'
 <div class="container">
     <div class="head">
         <div class="back">
-            <button style="float: left; border: none; font-size: 40px;"><i class="fas fa-reply"></i></button>
+            <button><i class="fas fa-reply"></i></button>
         </div>
         <div class="tittle text-center align-items-center col-sm-2 offset-sm-5" style="height: 50px;">
             <h2>Check Out</h2>
@@ -34,24 +34,21 @@ include '../dashboard/dashboardUser.php'
     <!-- Data detail pembelian user -->
     <div class="belanjaan col-sm-10 offset-sm-1 d-flex ">
         <img src="../img/vIWkY3LiPq.jpg" alt="">
-        <div class="detail" style="padding-left: 20px;">
+        <div class="detail col-sm-10" style="padding-left: 20px;">
             <h5>Kaus Hitam Latto</h5>
             <p>Rp. 250.000</p>
             <small>Ukuran : XL</small>
             <br>
             <small>Jumlah : 1 </small>
             <div class="border"></div>
-            <br>
-            <!-- TOLONG BUATIN GARIS BAWAH AKU MENYERAH!! DAN TOLONG BAGIAN SUB TOTAL HARGA BENERIN... BIAR NEMPEL KE SAMPING KANAN  -->
-            <div class="border" style="border: black solid 1px;">____________________________________________________
-            </div>
+            <hr>
             <div class="d-flex bd-highlight mb-3 ">
                 <div class="mr-auto p-2 bd-highlight">Sub Total</div>
                 <div class="p-2 bd-highlight">Rp. ........</div>
             </div>
         </div>
     </div>
-    <br><br>
+    <br>
 
     <div class="payment col-sm-10 offset-sm-1" style="padding: 20px; background-color: #DBDBDB;">
         <div class="">
@@ -69,7 +66,7 @@ include '../dashboard/dashboardUser.php'
                 </div>
                 <div class="p-2 bd-highlight">Rp. .........</div>
             </div>
-            <button type="button" class="offset-sm-11 col-sm-1 btn" style="background-color: #FFDCB4;">Pay</button>
+            <button type="button" class=" btn offset-sm-11 col-sm-1 btn" style="background-color: #FFDCB4;">Pay</button>
         </div>
     </div>
 
