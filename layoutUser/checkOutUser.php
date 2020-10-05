6bcb8cabc2f6cb5bf9901c8aa26b353bf95e66cd
Diky <juliusdikya@gmail.com>
2020-10-05 14:23:01 +0700

    update checkout

--- layoutUser/checkOutUser.php
+++ layoutUser/checkOutUser.php
@@ -1,78 +1,71 @@
 <?php
 include '../dashboard/dashboardUser.php'
 ?>
-<link rel="stylesheet" href="../css/checkOutUser.css">
-<br><br><br><br><br><br><br>
 
-<div class="container bg-abu">
-    <div class="head">
-        <div class="back">
-            <button><i class="fas fa-reply"></i></button>
+<div class="container content">
+    <div class="bg-abu">
+        <div class="head">
+            <div class="back">
+                <button><i class="fas fa-reply"></i></button>
+            </div>
+            <div class="tittle text-center align-items-center col-sm-2 offset-sm-5" style="height: auto; border-radius:10px">
+                <h3>Check Out</h3>
+            </div>
         </div>
-        <div class="tittle text-center align-items-center col-sm-2 offset-sm-5" style="height: 50px;">
-            <h2>Check Out</h2>
+        <br>
+
+
+        <!-- Data berupa alamat pembelian saat check out -->
+        <div class="dataUser col-sm-10 offset-sm-1"
+            style="background-color: #DBDBDB; border-radius: 10px; padding: 20px; margin-bottom: 20px;">
+            <p><strong>Alamat Pengiriman</strong></p>
+            <hr>
+            <p>Nama : </p>
+            <p>No. Telp : </p>
+            <p>Alamat :</p>
+            <p>Kota : </p>
         </div>
-    </div>
-    <br>
-
-
-    <!-- Data berupa alamat pembelian saat check out -->
-    <div class="dataUser col-sm-10 offset-sm-1"
-        style="background-color: #DBDBDB; border-radius: 10px; padding: 5px; margin-bottom: 20px;">
-        <p><strong>Alamat Pengiriman</strong></p>
-        <p><strong>Anoynimous</strong></p>
-        <p>Anoynimous <br>
-            08XXXXXXXXXXX <br>
-            Alamat <br>
-            Kota <br>
-        </p>
-    </div>
+       
 
 
     <!-- Butuh php  -->
     <!-- nanti pake foreach lalu data nya pake php -->
     <!-- Data detail pembelian user -->
-    <div class="belanjaan col-sm-10 offset-sm-1 d-flex ">
+    <div class="belanjaan col-sm-10 offset-sm-1 d-flex" style="padding:20px; margin-bottom:20px;" >
         <img src="../img/vIWkY3LiPq.jpg" alt="">
         <div class="detail col-sm-10" style="padding-left: 20px;">
-            <h5>Kaus Hitam Latto</h5>
-            <p>Rp. 250.000</p>
-            <small>Ukuran : XL</small>
-            <br>
-            <small>Jumlah : 1 </small>
+            <h5>Nama Barang</h5>
+            <p>Harga</p>
+            <p> Ukuran : </p>
+            <p> Jumlah : </p>
             <div class="border"></div>
-            <hr>
             <div class="d-flex bd-highlight mb-3 ">
                 <div class="mr-auto p-2 bd-highlight">Sub Total</div>
-                <div class="p-2 bd-highlight">Rp. ........</div>
+                <div class="p-2 bd-highlight">Total Harga</div>
             </div>
         </div>
     </div>
-    <br>
 
-    <div class="payment col-sm-10 offset-sm-1" style="padding: 20px; background-color: #DBDBDB;">
-        <div class="">
-            <div class="d-flex bd-highlight mb-3 ">
-                <div class="mr-auto p-2 bd-highlight">Total Belanja</div>
-                <div class="p-2 bd-highlight">Rp. .......</div>
-            </div>
-            <div class="d-flex bd-highlight mb-3 ">
-                <div class="mr-auto p-2 bd-highlight">Ongkos Kirim</div>
-                <div class="p-2 bd-highlight">Rp. ........</div>
-            </div>
-            <div class="d-flex bd-highlight mb-3 ">
-                <div class="mr-auto p-2 bd-highlight">
-                    <h5>Total Harga</h5>
-                </div>
-                <div class="p-2 bd-highlight">Rp. .........</div>
-            </div>
-            <button type="button" class=" btn offset-sm-11 col-sm-1 btn" style="background-color: #FFDCB4;">Pay</button>
+    <!--Batas Foreach -->
+
+    <div class="payment col-sm-10 offset-sm-1" style="padding:10px; margin-bottom:20px; background-color: #DBDBDB; border-radius:10px">
+        <div class="d-flex bd-highlight mb-3 ">
+            <div class="mr-auto p-2 bd-highlight">Total Belanja</div>
+            <div class="p-2 bd-highlight">Rp. .......</div>
         </div>
+        <div class="d-flex bd-highlight mb-3 ">
+            <div class="mr-auto p-2 bd-highlight">Ongkos Kirim</div>
+            <div class="p-2 bd-highlight">Rp. ........</div>
+        </div>
+        <div class="d-flex bd-highlight mb-3 ">
+            <div class="mr-auto p-2 bd-highlight">
+                <h5>Total Harga</h5>
+            </div>
+            <div class="p-2 bd-highlight">Rp. .........</div>
+        </div>
+        <button type="button" class=" btn offset-sm-11 col-sm-1 btn" style="background-color: #FFDCB4;">Pay</button>
     </div>
 
-    <br><br><br>
-    <br><br><br>
-    <br><br><br>
-
+    </div> 
 
 </div>
